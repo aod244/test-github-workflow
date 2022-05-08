8c86277ceb01daa010359ce23d2bfda7a34d3e45
Filip Ostrowski <91828926+aod244@users.noreply.github.com>
2022-05-08 10:58:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
+"""import flask"""
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    """""""
+    """Funkcja lint"""
     return "<h1>Hello WSB! Greetings from Flask!</h1>"
 if __name__ == "__main__":
     app.run(debug=True)
